6befd48bb3011e021a378be0038772b620e1ebb0
kayrahancoskun <kayrahancoskun10@gmail.com>
2024-06-26 21:36:37 +0300

    Add files via upload

---
+++ taahmin2.py
@@ -0,0 +1,14 @@
+import random
+print("Tahmin Oyununa Hoş Geldiniz")
+rastgele_sayi=random.randint(100,200)
+for i in range():
+    tahmin=int(input("100 ile 200 arasında aklınızdan bir sayı tutunuz"))
+if tahmin==rastgele_sayi:
+    print("Tebrikler,Doğru")
+    if tahmin<rastgele_sayi:
+        print("Daha Küçük Bir Sayı Seçiniz")
+        if tahmin>rastgele_sayi:
+            print("Daha Büyük Bir Sayı Seçiniz")        
+        print(rastgele_sayi)
+            
+
